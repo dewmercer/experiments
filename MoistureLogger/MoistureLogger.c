fd8a4d03d780116d147f7526438e6b0f45ac2c08
Dave <foo@bar.com>
2018-08-11 22:47:20 -0700

    Small refactoring

--- MoistureLogger/MoistureLogger.c
+++ MoistureLogger/MoistureLogger.c
@@ -1,64 +1,84 @@
 
 #include <wiringPi.h>
-#include <wiringPiI2C.h>
 #include <pcf8591.h>
+#include <mcp3004.h>
 #include <time.h>
-
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define ADAC_ADDRESS 0x48
+#define MCP3008
+#if defined(PCF8591)
+  #define RESOLUTION 8
+  #define SELECTOR 0x48
+  #define CHANNELS 4
+  int (*setup_ptr)(const int, const int) = &pcf8591Setup;
+#elif defined(MCP3008)
+  #define RESOLUTION 10
+  #define SELECTOR 0
+  #define CHANNELS 8
+  int (*setup_ptr)(const int, const int) = &mcp3004Setup;
+#else
+#error "No supported ADC is defined"
+#endif
 
-#define pinbase 64          //any number above 64
-#define A0 pinbase + 0
-#define A1 pinbase + 1
-
-#define SAMPLE_INTERVAL_SECONDS 60
+#define pinbase 64      
 
 #define DATA_FILE "/home/pi/moisture.log"
 
-#define TIME_BUFFER_SIZE 256
-char timeBuffer[TIME_BUFFER_SIZE];
-
 #define TIME_FORMAT "%F %T"
-#define LOG_FORMAT "%s, %2f, %2f\n"
+#define LOG_FORMAT 
 
-int main() {
-    int err = wiringPiSetup () ;
-    if (err != 0) {
-      printf("ERROR setting up wiringPi: %X\n", err);
-      return 1;
-    }    
-    printf("Wiring Pi Setup.\n");
+#define TIME_BUFFER_SIZE 256
 
-    err = pcf8591Setup(pinbase,ADAC_ADDRESS);
-    if (err < 0) {
-      printf("ERROR setting up I2C: %X\n", err);
-      return 1;
-    }    
-    printf("I2C Setup\n");
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Enter the number of seconds to pause between samples.\n");
+    return 1;
+  }
+  
+  int sampleInterval = atoi(argv[1]);
+  
+  printf("Sampeling every %d seconds\n", sampleInterval);
+  
+  const float DIVISOR = pow(2, RESOLUTION);
+  char timeBuffer[TIME_BUFFER_SIZE];
+  
+  int err = wiringPiSetup () ;
+  if (err != 0) {
+    printf("ERROR setting up wiringPi: %X\n", err);
+    return 1;
+  }    
+  printf("Wiring Pi Setup: %d\n", err);
+
+  err = (*setup_ptr)(pinbase, SELECTOR);
+  if (err == 0) {
+    printf("ERROR setting up ADC: %X\n", err);
+    return 1;
+  }    
+  printf("ADC Setup: %d\n", err);
     
-    time_t rawtime;
-    struct tm * timeinfo;
+  time_t rawtime;
+  struct tm * timeinfo;
 
-    int a0 = 0;
-    int a1 = 0;
-    while(1){
-        a0 = analogRead(A0);  //read A0 pin
-        a1 = analogRead(A1);  //read A0 pin
-        float val0 = (float)a0 / 255.0 * 5.0  ;  // calculate voltage
-        float val1 = (float)a1 / 255.0 * 5.0  ;  // calculate voltage
+  while(1){
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+    int len = strftime(timeBuffer, TIME_BUFFER_SIZE, TIME_FORMAT, timeinfo);
+    timeBuffer[len] = 0x00;
+    FILE* file = fopen(DATA_FILE, "a");
+    printf("%s", timeBuffer);
+    fprintf(file, "%s", timeBuffer);
         
-        time (&rawtime);
-        timeinfo = localtime (&rawtime);
-        int len = strftime(timeBuffer, TIME_BUFFER_SIZE, TIME_FORMAT, timeinfo);
-        timeBuffer[len] = 0x00;
-        
-        printf(LOG_FORMAT, timeBuffer, val0, val1);
-        FILE* file = fopen(DATA_FILE, "a");
-        fprintf(file, LOG_FORMAT, timeBuffer, val0, val1);
-        fclose(file);
-        delay(SAMPLE_INTERVAL_SECONDS * 1000);
+    for (int i = 0;i < CHANNELS; i++) {
+      int raw = analogRead(pinbase + i);
+      printf(", %.3f", (float)raw / DIVISOR * 5.0);
+      fprintf(file, ", %.3f", (float)raw / DIVISOR * 5.0);
     }
-    
-    return 0;
+    printf("\n");
+    fprintf(file, "\n");
+    fclose(file);
+    delay(sampleInterval * 1000);
+  }  
 }
+
